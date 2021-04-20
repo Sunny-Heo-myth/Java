3c3f708277b41ffab34887e3518247fac85a9791
Astronaut <hsymyth@gmail.com>
2021-04-21 00:13:17 +0900

    almost end

---
+++ collection/arraylist/MemberArrayListTest.java
@@ -0,0 +1,29 @@
+package collection.arraylist;
+
+import collection.Member;
+
+public class MemberArrayListTest {
+
+	public static void main(String[] args) {
+		MemberArrayList memberArrayList = new MemberArrayList();
+		
+		Member memberLee = new Member("Lee");
+		Member memberHeo = new Member("Heo");
+		Member memberKim = new Member("Kim");
+		Member memberHan = new Member("Han");
+		
+		memberArrayList.addMember(memberLee);
+		memberArrayList.addMember(memberHeo);
+		memberArrayList.addMember(memberKim);
+		memberArrayList.addMember(memberHan);
+		
+		memberArrayList.showAllMember();
+		
+		memberArrayList.removeMember(memberLee.getMemberID());
+		
+		memberArrayList.insertMember(memberLee, 4);
+		
+		memberArrayList.showAllMember();
+	}
+
+}
